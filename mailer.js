ac42e64de1e98cd86df6e366b454458b477a305f
Sebastian Lukas Hauer <sebastian.hauer@udo.edu>
2022-11-08 20:38:36 +0100

    cp error handling

--- mailer.js
+++ mailer.js
@@ -7,21 +7,27 @@ function recoveryMail(recepient, token) {
     config.mail_config.mail_program &&
     config.mail_config.mail_program !== ""
   ) {
-    const process = child_process.spawn(
-      `${config.mail_config.mail_program} -t`
-    );
-    process.stdin.write("From: " + config.mail_config.mail_from + "\n");
-    process.stdin.write("To: " + recepient + "\n");
-    process.stdin.write("Subject: Decker: Passwort zurücksetzen\n\n");
-    process.stdin.write(
-      `Guten Tag,\n    für Ihr Nutzerkonto wurde eine Anfrage zum Zurücksetzen des Passworts gestellt.\n\
-Zum zurücksetzen Ihres Passworts benutzen Sie bitte folgenden Link:\n\
-${config.hostname}/password-reset/${token}\n\
-Sollten Sie keine solche Anfrage gestellt haben können Sie diese E-Mail ignorieren.\n\n\
-Mit freundlichen Grüßen\n\
-    Das Decker System\n`
-    );
-    process.stdin.end();
+    try {
+      const process = child_process.spawn(
+        `${config.mail_config.mail_program}`,
+        ["-t"]
+      );
+      process.stdin.write("From: " + config.mail_config.mail_from + "\n");
+      process.stdin.write("To: " + recepient + "\n");
+      process.stdin.write("Subject: Decker: Passwort zurücksetzen\n\n");
+      process.stdin.write(
+        `Guten Tag,\n    für Ihr Nutzerkonto wurde eine Anfrage zum Zurücksetzen des Passworts gestellt.\n\
+  Zum zurücksetzen Ihres Passworts benutzen Sie bitte folgenden Link:\n\
+  ${config.hostname}/password-reset/${token}\n\
+  Sollten Sie keine solche Anfrage gestellt haben können Sie diese E-Mail ignorieren.\n\n\
+  Mit freundlichen Grüßen\n\
+      Das Decker System\n`
+      );
+      process.stdin.end();
+    } catch (error) {
+      console.error(error);
+      return;
+    }
   } else {
     console.error("Kein Mailprogramm konfiguriert.");
     return;
