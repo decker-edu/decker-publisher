dd34548017387d8653367aefaed5c309e201c111
Sebastian Lukas Hauer <sebastian.hauer@udo.edu>
2022-11-08 20:40:41 +0100

    typos

--- mailer.js
+++ mailer.js
@@ -14,14 +14,15 @@ function recoveryMail(recepient, token) {
       );
       process.stdin.write("From: " + config.mail_config.mail_from + "\n");
       process.stdin.write("To: " + recepient + "\n");
-      process.stdin.write("Subject: Decker: Passwort zurücksetzen\n\n");
+      process.stdin.write("Subject: Decker: Passwort zuruecksetzen\n\n");
       process.stdin.write(
-        `Guten Tag,\n    für Ihr Nutzerkonto wurde eine Anfrage zum Zurücksetzen des Passworts gestellt.\n\
-  Zum zurücksetzen Ihres Passworts benutzen Sie bitte folgenden Link:\n\
-  ${config.hostname}/password-reset/${token}\n\
-  Sollten Sie keine solche Anfrage gestellt haben können Sie diese E-Mail ignorieren.\n\n\
-  Mit freundlichen Grüßen\n\
-      Das Decker System\n`
+        `Guten Tag,\n\
+    für Ihr Nutzerkonto wurde eine Anfrage zum Zurücksetzen des Passworts gestellt.\n\
+Zum zurücksetzen Ihres Passworts benutzen Sie bitte folgenden Link:\n\
+${config.hostname}/password-reset/${token}\n\
+Sollten Sie keine solche Anfrage gestellt haben können Sie diese E-Mail ignorieren.\n\n\
+Mit freundlichen Grüßen\n\
+    Das Decker System\n`
       );
       process.stdin.end();
     } catch (error) {
