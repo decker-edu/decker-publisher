ac589f0a4c257b56adccc122855d1857d547db89
Sebastian Lukas Hauer <sebastian.hauer@udo.edu>
2022-11-30 01:23:17 +0100

    db requests

--- src/backend/request.ts
+++ src/backend/request.ts
@@ -4,11 +4,15 @@ class AccountRequest {
     id: number;
     username: string;
     email: string;
+    note: string;
+    token: string;
     
-    constructor(id: number, username: string, email: string) {
+    constructor(id: number, username: string, email: string, note: string, token: string) {
         this.id = id;
         this.username = username;
         this.email = email;
+        this.note = note;
+        this.token = token;
     }
 
     static async isAvailable(username: string) : Promise<boolean> {
@@ -33,12 +37,32 @@ class AccountRequest {
             const result = await database.query("INSERT INTO account_requests (token, username, email, created, note) VALUES ($1, $2, $3, NOW(), $4) RETURNING id", [token, username, email, note]);
             if(result.rows.length > 0) {
                 const id = result.rows[0];
+                return new AccountRequest(id, username, email, note, token);
             } else {
                 throw new Error("Unable to reserve account in the database.")
             }
         } catch (error) {
             console.error(error);
             throw error;
+        }
+    }
+
+    static async fromDatabase(token : string) : Promise<AccountRequest | null> {
+        try {
+            const result = await database.query("SELECT * FROM account_requests WHERE token = $1", [token]);
+            if(result.rows.length > 0) {
+                const entry = result.rows[0];
+                const username = entry.username;
+                const email = entry.email;
+                const note = entry.note;
+                const id = entry.id;
+                return new AccountRequest(id, username, email, note, token);
+            } else {
+                return null;
+            }
+        } catch (error) {
+            console.error(error);
+            return null;
         }
     }
 }
