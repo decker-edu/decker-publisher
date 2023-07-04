1a945dd55392b6976baed2e5e72ecaa72e284aeb
Sebastian Lukas Hauer <sebastian.hauer@udo.edu>
2023-07-05 01:26:58 +0200

    vttpath

--- src/frontend/routes/index.ts
+++ src/frontend/routes/index.ts
@@ -218,7 +218,7 @@ router.get(
             req.account.username,
             project,
             "files",
-            subtitles,
+            vttpath,
           ].join("/"),
       },
       glossaries: glossaries,
