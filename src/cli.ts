280f937bd4049c347be07c62154ecb674ccdb35c
Sebastian Lukas Hauer <sebastian.hauer@udo.edu>
2024-01-22 02:26:52 +0100

    improve cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,36 +1,147 @@
 require("module-alias/register");
 import database from "./backend/database";
+import { Account } from "./backend/account";
 
 import { exportFeedbackUsers } from "./backend/legacy.feedback";
 
-enum Operation {
-  PRINT_VERSION,
-  EXPORT_FEEDBACK,
+type Operation = {
+  fn: (...args: string[]) => Promise<void>;
+  argn: number;
+};
+
+const ops: Map<string, Operation> = new Map<string, Operation>();
+
+function registerOperation(arg: string, operation: Operation) {
+  ops.set(arg, operation);
 }
 
-let operation: Operation = undefined;
+registerOperation("--version", { fn: printVersion, argn: 0 });
+registerOperation("--list-feedback", {
+  fn: listFeedbackUsers,
+  argn: 0,
+});
+registerOperation("--list-accounts", {
+  fn: listAccounts,
+  argn: 0,
+});
+registerOperation("--list-requests", {
+  fn: listRequests,
+  argn: 0,
+});
+registerOperation("--export-feedback", { fn: exportFeedbackUsers, argn: 0 });
+registerOperation("--delete-feedback", { fn: deleteFeedbackUser, argn: 1 });
+registerOperation("--delete-account", { fn: deleteAccount, argn: 1 });
+registerOperation("--delete-request", { fn: deleteRequest, argn: 1 });
+registerOperation("--create-account", { fn: createAccount, argn: 3 });
 
-for (let index = 0; index < process.argv.length; index++) {
-  const arg = process.argv[index];
-  if (arg === "--version") {
-    operation = Operation.PRINT_VERSION;
-  } else if (arg === "--export-feedback") {
-    operation = Operation.EXPORT_FEEDBACK;
+main();
+
+async function readArguments() {
+  for (let index = 0; index < process.argv.length; index++) {
+    const arg = process.argv[index];
+    const op = ops.get(arg);
+    if (op) {
+      const args: string[] = [];
+      for (let i = 0; i < op.argn; i++) {
+        const argidx = index + i + 1;
+        if (argidx >= process.argv.length) {
+          console.error("Not enough arguments specified.");
+          return;
+        }
+        args.push(process.argv[argidx]);
+      }
+      await op.fn(...args);
+      index += op.argn;
+    }
   }
 }
 
-switch (operation) {
-  case Operation.PRINT_VERSION:
-    {
-      console.log("Version: 1.0.0");
-    }
-    break;
-  case Operation.EXPORT_FEEDBACK:
-    {
-      exportFeedbackUsers();
-    }
-    break;
-  default: {
-    console.error("No valid operation has been specified.");
+async function main() {
+  await readArguments();
+  await database.disconnect();
+}
+
+async function printVersion() {
+  console.log("Version: 1.0.0");
+}
+
+async function listAccounts() {
+  try {
+    const requests = await database.query(
+      "SELECT id, username, email, created FROM accounts"
+    );
+    const rows = requests.rows;
+    console.table(rows);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function listFeedbackUsers() {
+  try {
+    const requests = await database.query("SELECT * FROM feedback_accounts");
+    const rows = requests.rows;
+    console.table(rows);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function listRequests() {
+  try {
+    const requests = await database.query(
+      "SELECT id, username, email, token, note, created FROM account_requests"
+    );
+    const rows = requests.rows;
+    console.table(rows);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function deleteFeedbackUser(id: string) {
+  try {
+    const result = await database.query(
+      "DELETE FROM feedback_accounts WHERE id = $1",
+      [id]
+    );
+    console.log(`DELETED ${result.rowCount} feedback users.`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function deleteRequest(id: string) {
+  try {
+    const result = await database.query(
+      "DELETE FROM account_requests WHERE id = $1",
+      [id]
+    );
+    console.log(`DELETED ${result.rowCount} requests.`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function deleteAccount(id: string) {
+  try {
+    const result = await database.query("DELETE FROM accounts WHERE id = $1", [
+      id,
+    ]);
+    console.log(`DELETED ${result.rowCount} accounts.`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function createAccount(
+  username: string,
+  email: string,
+  password: string
+) {
+  try {
+    Account.register(username, email, password);
+  } catch (error) {
+    console.error(error);
   }
 }
--- static/javascripts/request.js
+++ static/javascripts/request.js
@@ -8,45 +8,64 @@ function closeRequestDialog() {
   dialog.close();
 }
 
-function displayRequestDialogMessage(message, type) {
-  let element = document.getElementById("request-message");
-  element.classList.remove("message-error");
-  element.classList.remove("message-success");
-  if (type && type === "success") {
-    element.classList.add("message-success");
-  } else if (type && type === "error") {
-    element.classList.add("message-error");
-  }
-  element.innerText = message;
+function openConfirmationDialog() {
+  let dialog = document.getElementById("confirmation-dialog");
+  dialog.showModal();
 }
 
-function asyncRequest() {
+function closeConfirmationDialog() {
+  let dialog = document.getElementById("confirmation-dialog");
+  dialog.close();
+}
+
+function displayRequestDialogMessage(message, type) {
+  let element;
+  if (type && type === "success") {
+    closeRequestDialog();
+    element = document.getElementById("confirmation-message");
+    element.classList.remove("message-error");
+    element.classList.remove("message-success");
+    element.classList.add("message-success");
+    element.innerText = message;
+    openConfirmationDialog();
+  } else if (type && type === "error") {
+    element = document.getElementById("request-message");
+    element.classList.remove("message-error");
+    element.classList.remove("message-success");
+    element.classList.add("message-error");
+    element.innerText = message;
+  }
+}
+
+async function postRequest() {
   let userElement = document.getElementById("request-user");
   let mailElement = document.getElementById("request-mail");
   let noteElement = document.getElementById("request-note");
   let username = userElement.value;
   let email = mailElement.value;
   let note = noteElement.value;
-  fetch("/api/request", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      requestUser: username,
-      requestMail: email,
-      requestNote: note,
-    }),
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      if (json.status === "success") {
-        displayRequestDialogMessage(json.message, "success");
-      } else {
-        displayRequestDialogMessage(json.message, "error");
-      }
+  try {
+    const response = await fetch("/api/request", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        requestUser: username,
+        requestMail: email,
+        requestNote: note,
+      }),
     });
+    if (response.ok) {
+      const json = await response.json();
+      displayRequestDialogMessage(json.message, "success");
+    } else {
+      const json = await response.json();
+      displayRequestDialogMessage(json.message, "error");
+    }
+  } catch (error) {
+    console.error(error);
+    displayRequestDialogMessage("Fehler während der Datenübertragung", "error");
+  }
 }
